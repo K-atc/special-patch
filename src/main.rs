7049a980fd1dcd7bb4aa7bcbe1ae0da2274ccc7e
K_atc <shiftx1026@gmail.com>
2022-08-18 15:40:55 +0900

    Update prallize patching

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use clap::Parser;
 #[allow(unused_imports)]
 use log::{info, trace};
+#[allow(unused_imports)]
+use rayon::prelude::*;
 use regex::Regex;
 use serde::Deserialize;
+use std::ffi::OsStr;
 use std::fs;
 use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 use std::path::PathBuf;
-use std::ffi::OsStr;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -34,11 +36,12 @@ fn main() {
         .expect(format!("Failed to open file: {:?}", &args.compile_commands).as_str());
     let compile_commands: Vec<CompileCommand> =
         serde_json::from_str(&compile_commands).expect("Failed to parse");
-    for command in compile_commands.iter() {
-        if Some("c") != command.file.extension().and_then(OsStr::to_str) {
-            continue;
-        }
+    assert!(compile_commands.len() > 0);
+    let _: Vec<()> = compile_commands.par_iter().map(|command| {
         trace!("{:?}", command.file);
+        if Some("c") != command.file.extension().and_then(OsStr::to_str) {
+            return;
+        }
 
         let re = Regex::new(r"([^\w^\(])NULL([^\w^\)])").unwrap();
         let original = fs::read_to_string(&command.file).expect("Failed to read");
@@ -49,5 +52,5 @@ fn main() {
             let mut writer = BufWriter::new(file);
             write!(writer, "{}", patched).expect("Failed to write patched code");
         }
-    }
+    }).collect();
 }
